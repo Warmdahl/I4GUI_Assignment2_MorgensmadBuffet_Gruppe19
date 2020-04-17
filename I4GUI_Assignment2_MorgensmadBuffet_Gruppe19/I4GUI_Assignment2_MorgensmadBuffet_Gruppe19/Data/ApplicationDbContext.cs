d9323139abc1b07b2f2beeebfaa16bb464a7262e
Warmdahl <emilwarmdahl@gmail.com>
2020-04-17 08:50:07 +0200

    Project made

---
+++ I4GUI_Assignment2_MorgensmadBuffet_Gruppe19/I4GUI_Assignment2_MorgensmadBuffet_Gruppe19/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace I4GUI_Assignment2_MorgensmadBuffet_Gruppe19.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ I4GUI_Assignment2_MorgensmadBuffet_Gruppe19/I4GUI_Assignment2_MorgensmadBuffet_Gruppe19/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace I4GUI_Assignment2_MorgensmadBuffet_Gruppe19.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
